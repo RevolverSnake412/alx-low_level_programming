cf4d911fbd5f270cb6a0499dcaf597ded814ac97
RevolverSnake412 <oussag@hotmail.fr>
2023-06-15 20:46:09 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,4 @@
  * Return: 0 on successful execution
  */
 int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle"); return (0);}
