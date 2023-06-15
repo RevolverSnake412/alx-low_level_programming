ee9da413d63e4fe00bce843d4634ac9dc243a1d1
RevolverSnake412 <oussag@hotmail.fr>
2023-06-15 21:15:00 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main ()
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
