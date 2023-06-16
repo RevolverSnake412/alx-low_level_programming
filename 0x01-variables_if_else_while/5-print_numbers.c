b8a2f50d6c2d53d8e8c0304cb55585985082ba1e
RevolverSnake412 <oussag@hotmail.fr>
2023-06-16 21:22:36 +0100

    Base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{int digit;
+for (digit = 0; digit <= 9 ; digit++)
+{printf("%d", digit);
+}
+puts("");
+return (0);
+}
