559b2c70f7b844efea9f8f9666884f0a6f3de496
RevolverSnake412 <oussag@hotmail.fr>
2023-06-16 22:34:36 +0100

    reversed alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+{putchar(letter);
+}
+putchar('\n')
+return (0);
+}
