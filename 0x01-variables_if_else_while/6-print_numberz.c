fb1941ecf76633775ca8b8f46e1ba731fc3a4e39
RevolverSnake412 <oussag@hotmail.fr>
2023-06-16 21:52:47 +0100

    Numberz uwu

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{char numberz[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+for (int index = 0; index <= 9; index++)
+{putchar(numberz[index]);
+}
+putchar('\n');
+return (0);
+}
