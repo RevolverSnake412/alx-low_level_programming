016b54e479881f841fe2193b386ecae37cf3416d
RevolverSnake412 <oussag@hotmail.fr>
2023-06-17 00:32:33 +0100

    All possible 3 digit combinations using base 10 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{int comb1 = 48;
+int comb2;
+int comb3;
+int mastercounter = 0;
+int counter2 = 0;
+int counter3 = 0;
+while (comb1 <= 55)
+{mastercounter++;
+comb2 = 49 + counter2;
+while (comb2 <= 56)
+{comb3 = 50 + counter3;
+while (comb3 <= 57)
+{putchar(comb1);
+putchar(comb2);
+putchar(comb3);
+if (mastercounter != 8)
+{putchar(',');
+putchar(32);
+}
+comb3++;
+}
+comb2++;
+counter3++;
+}
+counter3 = mastercounter;
+counter2++;
+comb1++;
+}
+}
