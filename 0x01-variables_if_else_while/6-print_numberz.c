1f965366ec1cc39c8143963ab8efe77145722378
RevolverSnake412 <oussag@hotmail.fr>
2023-06-16 22:07:42 +0100

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,7 +6,7 @@
  * Return: 0 on successful execution
  */
 int main(void)
-{char numberz[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+{int numberz[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 int index;
 for (index = 0; index <= 9; index++)
 {putchar(numberz[index]);
