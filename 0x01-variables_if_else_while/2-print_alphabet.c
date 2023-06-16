e3cb28d2b8167c6e76eefa5ff24ab7cdc6e2a5a7
RevolverSnake412 <oussag@hotmail.fr>
2023-06-16 21:00:19 +0100

    lowercased alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main()
+{char letter;
+for (letter = 'a'; letter <='z'; letter++)
+{putchar(letter);
+}
+return (0);
+}
