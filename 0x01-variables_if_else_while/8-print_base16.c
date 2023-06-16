c5e71b15e7b53f6d0d71f33d343c99284fffbeff
RevolverSnake412 <oussag@hotmail.fr>
2023-06-16 22:51:31 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
+int main(void){
+int hex;
+for (hex = 48; hex <= 102; hex++)
+{
+if (hex > 57 && hex < 97)
+{continue;
+}
+else
+{putchar(hex);
+}
+}
+}
