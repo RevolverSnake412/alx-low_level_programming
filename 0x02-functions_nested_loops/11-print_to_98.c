ad78e9e206be51852f53077f0d9955e1ec242130
RevolverSnake412 <oussag@hotmail.fr>
2023-06-21 00:48:21 +0100

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,33 +7,34 @@
  * Return: 0 when successful
 */
 int print_to_98(int n)
-{
+{i = n;
+int number = i;
+int digits[3];
 if (n <= 98)
 {
-for (int i = n; i <= 98; i++)
+for (i; i <= 98; i++)
 {
 if (i != n)
 {putchar(44);
 putchar(32);
 }
-int number = i;
 if (number < 0)
 {putchar(45);
 number *= -1;
 }
-int digits[3];
 int count = 0;
 do {digits[count++] = number % 10;
 number /= 10;
 } while (number > 0);
-for (int j = count - 1; j >= 0; j--)
+int j = count - 1;
+for (j; j >= 0; j--)
 {putchar(digits[j] + 48);
 }
 }
 }
 else
 {
-for (int i = n; i >= 98; i--)
+for (i; i >= 98; i--)
 {
 if (i != n)
 {putchar(44);
