3565daaf3e9acf38327150b3fd7cbfedfe438a2a
RevolverSnake412 <oussag@hotmail.fr>
2023-06-21 00:03:55 +0100

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,29 +5,10 @@
  * @a: int
  * @b: int
  *
- * Return: 0 when success
+ * Return: result
 */
 #include <stdio.h>
 int add(int a, int b)
-{int sum = a + b;
-int sign = 1;
-int result = 0;
-if (sum < 0)
-{sum = -sum;
-sign = -1;
-}
-while (sum != 0)
-{int digit = sum % 10;
-result = result * 10 + digit;
-sum /= 10;
-}
-if (sign == -1)
-{putchar('-');
-}
-while (result != 0)
-{int digit = result % 10;
-putchar(digit + '0');
-result /= 10;
-}
-return (0);
+{int result = a + b;
+return (result);
 }
