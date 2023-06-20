8cb18aff5cc2456c8ebdeecbe0a207e048c57bd1
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 20:12:26 +0100

    pos || neg || zero?

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - is n pos || neg || zero?
+ *
+ * @n: int
+ *
+ * Return: 0 if zero, 1 if positive, -1 if negative
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{_putchar(43);
+return (1);
+}
+else if (n < 0)
+{_putchar(45);
+return(-1);
+}
+else
+{_putchar(48);
+return(0);
+}
+}
