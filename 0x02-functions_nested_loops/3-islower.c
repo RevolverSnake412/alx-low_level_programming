b09ce6253c6e4e638f7c0f6c1d5172de2588ce18
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 19:32:13 +0100

    lowercase or no?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * islower - checks if lowercase or no
+ *
+ * Return: 0 on success
+*/
+int islower(int c)
+{
+if (islower(c))
+{_putchar(49);
+}
+else
+{_putchar(48);
+}
+_putchar('\n');
+return (0);
+}
