88111191bb14d00428c1ef7cfe402651184b5b74
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 18:55:24 +0100

    owo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/** 
+ * print_alphabet - from a to z lowercased
+ *
+ * return: 0 on success
+*/
+int print_alphabet(void)
+{int i;
+for (i = 97; i <= 122; i++)
+{_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
