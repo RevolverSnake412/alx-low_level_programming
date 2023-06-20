eeac9e077f1d7e4a1b5f656574ceb6f70cd10a45
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 18:57:43 +0100

    owo

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet - from a to z lowercased
- *
- * return: 0 on success
 */
 int print_alphabet(void)
 {int i;
@@ -10,5 +8,4 @@ for (i = 97; i <= 122; i++)
 {_putchar(i);
 }
 _putchar('\n');
-return (0);
 }
