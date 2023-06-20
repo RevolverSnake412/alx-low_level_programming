97b46c6e8105ed6bd6a12c7c78c5e43fa6da4c06
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 20:02:32 +0100

    checks if the character is letter or no

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - check if character is a letter or no
+ *
+ *
+ * @c: int
+ *
+ * Return: 0 if not a letter, 1 if letter
+*/
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+{return (1);
+}
+else
+{return (0);
+}
+}
