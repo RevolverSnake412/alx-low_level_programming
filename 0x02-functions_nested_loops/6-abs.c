b21d0c0bfea91c6b9456b73f0c2ffedd1dbf39e7
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 20:24:24 +0100

    computes the absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,18 @@
 /**
  * _abs - computes the absolute
  *
- * @r: int
+ * @int: int
  *
  * Return: r, 0 on success
 */
-int _abs(int r)
+int _abs(int)
 {
 if (r < 0)
-{r*=-1;
+{r *= -1;
 return (r);
+}
+else if (r > 0)
+{return (r);
 }
 return (0);
 }
