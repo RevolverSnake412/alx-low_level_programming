045201881f319e0e69c01cf8711e99c25e43c8d1
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 23:05:31 +0100

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,15 +14,15 @@ int xTenDigit = number / 10;
 int xOneDigit = number % 10;
 if (xTenDigit == 0)
 {_putchar(xOneDigit + 48);
-_putchar(32);
 }
 else
 {_putchar(xTenDigit + 48);
 _putchar(xOneDigit + 48);
-_putchar(32);
 }
 if (j != 9)
 {_putchar(44);
+_putchar(32);
+_putchar(32);
 }
 }
 _putchar('\n');
