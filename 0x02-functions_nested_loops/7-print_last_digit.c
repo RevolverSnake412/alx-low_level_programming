c844bce93adeab83bf7b890462855aeac8d59c70
RevolverSnake412 <oussag@hotmail.fr>
2023-06-20 20:34:05 +0100

    take the last digit of any integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of an integer
+ *
+ * @r: int
+ *
+ * Return: r, 0 on success
+*/
+int print_last_digit(int r)
+{r %= 10;
+return (r);
+return (0);
+}
