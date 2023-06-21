a36108cf18f954e6b3a81e57cb34bc41f17be985
RevolverSnake412 <oussag@hotmail.fr>
2023-06-21 01:51:04 +0100

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,68 +4,27 @@
  *
  * @n: int
  *
- * Return: 0 when success
-*/
+ * Return: 0 if successful
+ */
 int print_to_98(int n)
 {
 if (n >= 98)
 {
-for (; n >= 98; n--)
-{
-if (n >= 100)
-{_putchar((n / 100) + 48);
-_putchar(((n / 10) % 10) + 48);
-_putchar((n % 10) + 48);
-}
-else
-{
-_putchar((n / 10) + 48);
-_putchar((n % 10) + 48);
-}
+while (n >= 98)
+{printf("%d", n--);
 if (n != 98)
 {_putchar(44);
 _putchar(32);
 }
-else
-{_putchar('\n');
 }
-}
-}
-else if (n <= 98)
-{
-for (; n <= 98; n++)
-{
-if (n < 0)
-{int neg = n * -1;
-_putchar(45);
-if (neg >= 100)
-{
-_putchar((neg / 100) + 48);
-_putchar(((neg / 10) % 10) + 48);
-_putchar((neg % 10) + 48);
 }
 else
 {
-_putchar((neg / 10) + 48);
-_putchar((neg % 10) + 48);
-}
-}
-else if (n <= 9)
-{
-_putchar(n + 48);
-}
-else if (n >= 10)
-{
-_putchar((n / 10) + 48);
-_putchar((n % 10) + 48);
-}
+while (n <= 98)
+{printf("%d", n++);
 if (n != 98)
-{
-_putchar(44);
+{_putchar(44);
 _putchar(32);
-}
-else
-{_putchar('\n');
 }
 }
 }
