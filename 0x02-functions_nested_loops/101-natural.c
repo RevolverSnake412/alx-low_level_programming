66f71b8b9a99ab421497c144879ab8d52f6ac5ed
RevolverSnake412 <oussag@hotmail.fr>
2023-06-21 20:38:29 +0100

    sum of numbers divided by 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: 0 on success
+*/
+void main(void)
+{int i, s = 0;
+for (i = 1024; i != 1; i--)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+{
+s += i;
+}
+else
+{continue;
+}
+}
+putchar((s / 100000) + 48);
+putchar(((s / 10000) % 10) + 48);
+putchar(((s / 1000) % 10) + 48);
+putchar(((s / 100) % 10) + 48);
+putchar(((s / 10) % 10) + 48);
+putchar((s % 10) + 48);
+putchar('\n');
+return (0);
+}
