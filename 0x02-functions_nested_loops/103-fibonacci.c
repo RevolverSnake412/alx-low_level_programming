95e83c085c38bb1e4a57ca4abe92cbb3b8676171
RevolverSnake412 <oussag@hotmail.fr>
2023-06-21 21:20:15 +0100

    ,,uwu,,

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: 0 on return
+*/
+int main(void)
+{unsigned long int a = 1, b = 2, s, result, i;
+for (i = 1; i != 33; i++)
+{
+if (a < 4000000 && (a % 2) == 0)
+{
+result = result + a;
+}
+s = a + b;
+a = b;
+b = s;
+}
+printf("%lu", result);
+putchar('\n');
+return (0);
+}
