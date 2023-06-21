1bcf964d03dcb145b67b6e4c5b8dc5f36b75eade
RevolverSnake412 <oussag@hotmail.fr>
2023-06-21 19:33:37 +0100

    uwu

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,34 +10,34 @@ int print_times_table(int n)
 {int i, j;
 for (i = 0; i <= n; i++)
 {j = 1;
-putchar(48);
-putchar(44);
-putchar(32);
+_putchar(48);
+_putchar(44);
+_putchar(32);
 for (; j <= n; j++)
 {int result = i * j;
 if (result >= 100)
-{putchar((result / 100) + 48);
-putchar(((result / 10) % 10) + 48);
-putchar((result % 10) + 48);
+{_putchar((result / 100) + 48);
+_putchar(((result / 10) % 10) + 48);
+_putchar((result % 10) + 48);
 }
 else if (result >= 10 && result <= 99)
-{putchar(32);
-putchar((result / 10) + 48);
-putchar((result % 10) + 48);
+{_putchar(32);
+_putchar((result / 10) + 48);
+_putchar((result % 10) + 48);
 }
 else
-{putchar(32);
-putchar(32);
-putchar(result + 48);
+{_putchar(32);
+_putchar(32);
+_putchar(result + 48);
 }
 if (j != n)
-{putchar(44);
-putchar(32);
+{_putchar(44);
+_putchar(32);
 }
 }
 if (i != n)
 {
-putchar('\n');
+_putchar('\n');
 }
 }
 return (0);
