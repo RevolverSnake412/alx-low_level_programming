5669c72cc0842d271ccba81a1e2fec13d6c0a3e3
RevolverSnake412 <oussag@hotmail.fr>
2023-06-21 01:36:56 +0100

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,22 @@
  * Return: 0 when success
 */
 int print_to_98(int n)
-{
+{int i = n, j = n;
 if (n >= 98)
 {
-for (n; n >= 98; n--)
+for (i; i >= 98; i--)
 {
-if (n >= 100)
-{_putchar((n / 100) + 48);
-_putchar(((n / 10) % 10) + 48);
-_putchar((n % 10) + 48);
+if (i >= 100)
+{_putchar((i / 100) + 48);
+_putchar(((i / 10) % 10) + 48);
+_putchar((i % 10) + 48);
 }
 else
 {
-_putchar((n / 10) + 48);
-_putchar((n % 10) + 48);
+_putchar((i / 10) + 48);
+_putchar((i % 10) + 48);
 }
-if (n != 98)
+if (i != 98)
 {_putchar(44);
 _putchar(32);
 }
@@ -30,10 +30,10 @@ _putchar(32);
 }
 else if (n <= 98)
 {
-for (n; n <= 98; n++)
+for (j; j <= 98; j++)
 {
-if (n < 0)
-{int neg = n * -1;
+if (j < 0)
+{int neg = j * -1;
 _putchar(45);
 if (neg >= 100)
 {
@@ -47,16 +47,16 @@ _putchar((neg / 10) + 48);
 _putchar((neg % 10) + 48);
 }
 }
-else if (n <= 9)
+else if (j <= 9)
 {
-_putchar(n + 48);
+_putchar(j + 48);
 }
-else if (n >= 10)
+else if (j >= 10)
 {
-_putchar((n / 10) + 48);
-_putchar((n % 10) + 48);
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
 }
-if (n != 98)
+if (j != 98)
 {
 _putchar(44);
 _putchar(32);
