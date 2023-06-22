83cb2ca64aec648608ce127e4c748c033257a55f
RevolverSnake412 <oussag@hotmail.fr>
2023-06-22 22:54:49 +0100

    Fizz Buzz :D

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - main
+ * main - Fizz Buzz :D
  *
  * Return: 0 on success
 */
@@ -8,17 +8,17 @@ int main(void)
 {	int i = 1;
 	for (; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
