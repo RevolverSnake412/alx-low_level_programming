8487a9eb248fa6840f734e22eefe25b5736b0449
RevolverSnake412 <oussag@hotmail.fr>
2023-06-23 00:05:07 +0100

    from 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,10 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 			{
-				_putchar('1');
+				_putchar((j / 10) + 48);
 			}
-			_putchar(j % 10 + '0');
+			_putchar((j % 10) + 48);
 		}
-	}
 	_putchar('\n');
+	}
 }
