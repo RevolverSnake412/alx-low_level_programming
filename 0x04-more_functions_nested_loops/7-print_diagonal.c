110c7b5b6a6d488bfddd5ab3942886b7744d5125
RevolverSnake412 <oussag@hotmail.fr>
2023-06-22 22:20:58 +0100

    diagonal line drawer ^o^

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+*/
+void print_diagonal(int n)
+{int i = 1, j, count = -1;
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			if (i != 1)
+			{
+				_putchar('\n');
+			}
+			for (j = 0; j <= count; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			i++;
+			count++;
+		}
+	}
+	_putchar('\n');
+}
