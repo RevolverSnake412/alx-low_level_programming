cb2a3d3e176abee734d8faab283096d93632fa19
RevolverSnake412 <oussag@hotmail.fr>
2023-06-22 22:43:07 +0100

    fizz buzz :D

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main
+*/
+int main (void)
+{	int i = 1;
+	for (; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			putchar(32);
+		}
+	}
+	putchar('\n');
+}
