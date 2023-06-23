75ee0c23f416c498a0672d87f1029cb591e9d92a
RevolverSnake412 <oussag@hotmail.fr>
2023-06-23 02:45:48 +0100

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,32 +5,16 @@
  * @n: the number provided by the user
 */
 void print_number(int n)
-{	int zero = 1;
-	int temp = n;
-
-	if (n == 0)
+{
+	if (n <= -1)
 	{
-	_putchar(48);
-	return;
-	}
-	if (n < 0)
-	{
-		_putchar(45);
 		n *= -1;
+		_putchar(45);
 	}
-	while (temp != 0)
+	if (n / 10)
 	{
-	zero *= 10;
-	temp /= 10;
+		print_number(n / 10);
 	}
-	zero /= 10;
-	while (zero != 0)
-	{
-	int digit = n / zero;
-
-	_putchar('0' + digit);
-	n %= zero;
-	zero /= 10;
-	}
+	_putchar((n % 10) + 48);
 }
 
