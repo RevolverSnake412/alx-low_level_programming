ee715c87127df5edde1abebee2a27c255c4ea466
RevolverSnake412 <oussag@hotmail.fr>
2023-06-26 09:05:48 +0100

    prints string's len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - prints string's len
+ *
+ * @s: char
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+    	}
+	return len;
+}
