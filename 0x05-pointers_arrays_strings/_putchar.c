84fd3e0702616edf127d2f862d34447f571194b8
RevolverSnake412 <oussag@hotmail.fr>
2023-06-26 09:18:19 +0100

    Prints text to user

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - displays 1 char
+ *
+ * @c: char
+ *
+ * Return: character
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
