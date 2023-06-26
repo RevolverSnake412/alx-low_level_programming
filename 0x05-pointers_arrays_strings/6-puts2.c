ce1e74c50627544b5108d0132ff367334b880b6d
RevolverSnake412 <oussag@hotmail.fr>
2023-06-26 09:57:01 +0100

    prints even numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints even numbers
+ *
+ * @str: char
+*/
+void puts2(char *str)
+{	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+	}
+	_putchar('\n');
+}
