20df13951b0088910594c2a6c64507752a0b6aa1
RevolverSnake412 <oussag@hotmail.fr>
2023-06-26 09:45:57 +0100

    text reversed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses string
+ *
+ * @s: char
+*/
+void rev_string(char *s)
+{
+	int index = 0;
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	char text;
+	int i = 0;
+	for (; i < index; i++)
+	{
+		index--;
+		text = s[i];
+		s[i] = s[index];
+		s[index] = text;
+	}
+}
