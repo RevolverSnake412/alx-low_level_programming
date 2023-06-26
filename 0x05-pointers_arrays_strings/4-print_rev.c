1d520c8d95f9795d739ac4d1f692823bd50d614b
RevolverSnake412 <oussag@hotmail.fr>
2023-06-26 09:37:51 +0100

    reverse texting

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,10 @@ void print_rev(char *s)
 {	int count = 0;
 	while (*s != '\0')
 	{
-		if (count != 0)
-		{
-			s++;
-		}
+		s++;
 		count++;
 	}
+	s--;
 	for (; count > 0; count--)
 	{
 		_putchar(*s);
