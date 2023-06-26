5900c4312cfdb6e8d37276b6d00029acac5851f9
RevolverSnake412 <oussag@hotmail.fr>
2023-06-26 10:48:52 +0100

    prints elemets of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints all arrays
+ *
+ * @a: int
+ *
+ * @n: int
+*/
+void print_array(int *a, int n)
+{	int i = 0;
+	for (; i < n - 1; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			_putchar(44);
+			_putchar(32);
+		}
+	}
+	_putchar('\n');
+}
