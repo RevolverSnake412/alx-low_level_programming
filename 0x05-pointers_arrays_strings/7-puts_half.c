5ad98f3322656768a756674e9fe91076d18596e8
RevolverSnake412 <oussag@hotmail.fr>
2023-06-26 10:15:30 +0100

    half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,14 +5,13 @@
  * @str: char
 */
 void puts_half(char *str)
-{	int counter = 0;
+{	int counter = 0, halfcounter;
 	while (str[counter] != '\0')
 	{
-		str++;
 		counter++;
 	}
-	counter /= 2;
-	for (; counter > 0; counter--)
+	halfcounter = counter / 2;
+	for (; counter != halfcounter; counter--)
 	{
 		_putchar(str[counter]);
 	}
