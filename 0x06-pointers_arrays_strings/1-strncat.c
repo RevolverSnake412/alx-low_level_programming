3294c6f018e268579231db322cc5c7fe45649fa1
RevolverSnake412 <oussag@hotmail.fr>
2023-06-29 17:34:11 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,8 +11,7 @@
  * Return: dest
 */
 char *_strncat(char *dest, char *src, int n)
-{
-        int i = 0, j = 0;
+{	 int i = 0, j = 0;
         while (dest[i] != '\0')
         {
                 i++;
