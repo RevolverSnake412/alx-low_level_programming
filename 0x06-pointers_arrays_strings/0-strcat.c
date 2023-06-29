116a530ceff6a9e5170e61ca23673c02d6a0a39f
RevolverSnake412 <oussag@hotmail.fr>
2023-06-29 15:22:12 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,16 @@
 */
 char _strcat(char *dest, char *src)
 {	int i = 0, j = 0;
-	while (dest[j] != '/0')
+	while (dest[j] != '\0')
 	{
 		i++;
 	}
-	while (src[j] != '/0')
+	while (src[j] != '\0')
 	{
-		dest[i] = src [j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '/0';
+	dest[i] = '\0';
 	return (dest);
 }
