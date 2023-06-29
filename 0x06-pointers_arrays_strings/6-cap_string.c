1a801cc32e6c036bb8f6dd457ddc7bebe8ee9c21
RevolverSnake412 <oussag@hotmail.fr>
2023-06-29 21:50:57 +0100

    caps

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,29 +7,31 @@
  * Return: str
 */
 char *cap_string(char *str)
-{int i = 0, j;
+{int i = 0;
 
-while (str [i] != '\0')
-{i++;
-}
-for (j = 0; j <= i; j++)
+while (str [i])
 {
-if (i == 0 ||
-str[i] == 32 ||
-str[i] == ',' ||
-str[i] == ';' ||
-str[i] == '.' ||
-str[i] == '!' ||
-str[i] == '?' ||
-str[i] == '"' ||
-str[i] == '(' ||
-str[i] == ')' ||
-str[i] == '{' ||
-str[i] == '}' ||
-str[i] == '\t' ||
-str[i] == '\n' ||)
-{str[i+1] -= 32;
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+{
+i++;
 }
+if (i == 0 ||
+str[i - 1] == ' ' ||
+str[i - 1] == ',' ||
+str[i - 1] == ';' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n')
+{str[i] -= 32;
+}
+i++;
 }
 return (str);
 }
