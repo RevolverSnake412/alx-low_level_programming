1189ec9a2e5421fa828e6f8e0d40543c22481e47
RevolverSnake412 <oussag@hotmail.fr>
2023-06-29 19:16:03 +0100

    fix syntax

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,12 @@
 char *string_toupper(char *str)
 {int i = 0, j;
 
-while (str[i] != '\0'
+while (str[i] != '\0')
 {i++;
 }
 for (j = 0, j <= i, j++)
 {
-if (str[j] <= 122 && str[j] >= 97
+if (str[j] <= 122 && str[j] >= 97)
 {str[j] -= 32;
 }
 }
