8f6b58a13499a2c8097dd426b6f21ecda923e6a6
RevolverSnake412 <oussag@hotmail.fr>
2023-06-30 02:30:40 +0100

    print nums

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,16 @@
  * Return: 0
 */
 void print_number(int n)
-{	unsigned int digit;
-        digit = n;
-        if (n <= 1)
-        {
-                _putchar(45);
-                digit *= -1;
-        }
-        if (digit / 10 != 0)
-        {
-                print_number(digit / 10);
-        }
-        _putchar((digit % 10) + 48);
+{unsigned int digit;
+digit = n;
+if (n < 0)
+{
+_putchar('-');
+digit *= -1;
+}
+if (digit / 10 != 0)
+{
+print_number(digit / 10);
+}
+_putchar((digit % 10) + '0');
 }
