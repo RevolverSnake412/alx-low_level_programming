06cb566076b486ee59771a134217cd759d0504a6
RevolverSnake412 <oussag@hotmail.fr>
2023-06-30 02:22:54 +0100

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,19 +7,19 @@
  * Return: str on success
 */
 char *rot13(char *str)
-{	int i = 0, j;
-	char normal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		for (j = 0; j < 52; j++)
-                {
-                        if (str[i] == normal[j])
-                        {
-				str[i] = rot13[j];
-				break;
-			}
-		}
-	}
-	return (str);
+{int i = 0, j;
+char normal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (str[i] == normal[j])
+{
+str[i] = rot13[j];
+break;
+}
+}
+}
+return (str);
 }
