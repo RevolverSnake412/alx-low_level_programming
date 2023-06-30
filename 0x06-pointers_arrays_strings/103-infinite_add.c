76cb0a3a6f4e5d76d96c4585131c1c74fc903693
RevolverSnake412 <oussag@hotmail.fr>
2023-06-30 03:08:23 +0100

    add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -20,6 +20,7 @@ temp = *(n + j);
 *(n + i) = temp;
 }
 }
+
 /**
  * infinite_add - infinite add
  * @n1: char
@@ -33,7 +34,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 int val1 = 0, val2 = 0, tot = 0;
 
 while (*(n1 + i) != '\0')
-{                                                               i++;
+{
+i++;
 }
 while (*(n2 + j) != '\0')
 {
@@ -41,18 +43,23 @@ j++;
 }
 i--;
 j--;
-if (j >= size_r || i >= size_r)                                 {
+
+if (j >= size_r || i >= size_r)
+{
 return (0);
 }
+
 while (j >= 0 || i >= 0 || lever == 1)
 {
-if (i < 0)                                                      {
+if (i < 0)
+{
 val1 = 0;
 }
 else
 {
 val1 = *(n1 + i) - '0';
 }
+
 if (j < 0)
 {
 val2 = 0;
@@ -61,7 +68,9 @@ else
 {
 val2 = *(n2 + j) - '0';
 }
+
 tot = val1 + val2 + lever;
+
 if (tot >= 10)
 {
 lever = 1;
@@ -70,18 +79,23 @@ else
 {
 lever = 0;
 }
+
 if (digits >= (size_r - 1))
 {
 return (0);
 }
-*(r + digits) = (tot % 10) + '0';                               digits++;
+
+*(r + digits) = (tot % 10) + '0';
+digits++;
 j--;
 i--;
 }
+
 if (digits == size_r)
 {
 return (0);
 }
+
 *(r + digits) = '\0';
 rev_string(r);
 return (r);
