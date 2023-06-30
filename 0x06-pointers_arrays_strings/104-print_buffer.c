f7c9ba2a076ded25364eb76fe724671e9871c58b
RevolverSnake412 <oussag@hotmail.fr>
2023-06-30 03:19:17 +0100

    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

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,38 +8,37 @@
  * @size: int
 */
 void print_buffer(char *b, int size)
+{int i, j, check = 0;
+if (size <= 0)
 {
-        int i, j, check = 0;
-        if (size <= 0)
-        {
-		printf("\n");
-                return;
-        }
-        while (check < size)
-        {
-                j = size - check < 10 ? size - check : 10;
-                printf("%08x: ", check);
-                for (i = 0; i < 10; i++)
-                {
-			if (i < j)
-                                printf("%02x", *(b + check + i));
-                        else
-                                printf("  ");
-                        if (i % 2)
-                        {
-                                printf(" ");
-                        }
-                }
-                for (i = 0; i < j; i++)
-                {
-                        int c = *(b + check + i);
-                        if (c < 32 || c > 132)
-                        {
-                                c = '.';
-                        }
-                        printf("%c", c);
-                }
-                printf("\n");
-                check += 10;
-        }
+printf("\n");
+return;
+}
+while (check < size)
+{
+j = size - check < 10 ? size - check : 10;
+printf("%08x: ", check);
+for (i = 0; i < 10; i++)
+{
+if (i < j)
+printf("%02x", *(b + check + i));
+else
+printf("  ");
+if (i % 2)
+{
+printf(" ");
+}
+}
+for (i = 0; i < j; i++)
+{
+int c = *(b + check + i);
+if (c < 32 || c > 132)
+{
+c = '.';
+}
+printf("%c", c);
+}
+printf("\n");
+check += 10;
+}
 }
