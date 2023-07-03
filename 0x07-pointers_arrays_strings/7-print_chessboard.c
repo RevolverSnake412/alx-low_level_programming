efeda064e6c8eb3a7e6c32c6d6da80cbd9dfae68
RevolverSnake412 <oussag@hotmail.fr>
2023-07-03 19:48:25 +0100

    cheese bored

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_chessboard - print_chessboard
+ *
+ * @a: char
+ *
+ * Return: 0 on success
+*/
+void print_chessboard(char (*a)[8])
+{	int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (a[i][j] == ' ')
+			{
+				break;
+			}
+			else
+			{
+				_putchar(a[i][j]);
+			}
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
