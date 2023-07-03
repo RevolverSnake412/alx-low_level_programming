af8eeead281983f657bec96ba848b8bafff07cac
RevolverSnake412 <oussag@hotmail.fr>
2023-07-03 19:27:53 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.c"
+/**
+ * _strstr - strstr
+ *
+ * @haystack: char
+ *
+ * @needle: char
+ *
+ * Return: haystack
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack >= '\0')
+	{
+		char *a = needle;
+		char *b = haystack;
+
+		haystack++;
+
+		while (*a == *b && *a != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*a == '\0')
+		{
+			return (haystack);
+		}
+	}
+}
