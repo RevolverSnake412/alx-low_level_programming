849126d9c828a23688a43077ae36fdd4a75cdc53
RevolverSnake412 <oussag@hotmail.fr>
2023-07-03 19:55:49 +0100

    cheese bored

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,14 +13,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j <= 7; j++)
 		{
-			if (a[i][j] == ' ')
-			{
-				break;
-			}
-			else
-			{
-				_putchar(a[i][j]);
-			}
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
