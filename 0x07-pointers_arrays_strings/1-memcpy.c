ef7d05847972267cf72deb36807d4b14ed95c1f8
RevolverSnake412 <oussag@hotmail.fr>
2023-07-03 16:38:15 +0100

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.hi"
+/**
+ * _memcpy - memcpy
+ *
+ * @dest: char
+ *
+ * @src: char
+ *
+ * @n: int
+ *
+ * Return: str
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{	int i;
+	while (n > 0)
+	{
+		dest[i] = src[i];
+		i++;
+		n--;
+	}
+	return (src);
+}
