0a68acbf1020bd5bf67db7b0cf1dbfa9e6939cfb
RevolverSnake412 <oussag@hotmail.fr>
2023-07-05 16:17:28 +0100

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * _pow_recursion - pow
+ *
+ * @x: int
+ *
+ * @y: int
+ *
+ * Return: -1 if y less than zero, 1 if y equals to 1, results
+*/
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
