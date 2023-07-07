fd006cbaa18cd41b63df06117e7d2af2fbdfa2a2
RevolverSnake412 <oussag@hotmail.fr>
2023-07-07 18:03:42 +0100

    say my name... you're godda** right

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 /**
+ * main - main
  *
+ * @argc: index
+ * @argv: arguments in string
+ *
+ * Return: 0 on success
 */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
