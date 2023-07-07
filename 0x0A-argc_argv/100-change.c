060d02a77af98cebbba3b725c6174bf225b6421c
RevolverSnake412 <oussag@hotmail.fr>
2023-07-07 21:12:30 +0100

    changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,34 +6,38 @@
  * @argc: index
  * @argv: args' array
  *
- * Return: 1 if fails, 0 on success
+ * Return: 1 fail, 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int Cents, S = 0;
+	int money, i, result;
 	int coins[] = {25, 10, 5, 2, 1};
-	int Money = atoi(argv[1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (Money < 0)
+
+	money = atoi(argv[1]);
+	result = 0;
+
+	if (money < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	for (Cents = 0; Cents < 5 && Money >= 0; Cents++)
+	for (i = 0; i < 5 && money >= 0; i++)
 	{
-		while (Money >= coins[Cents])
+		while (money >= coins[i])
 		{
-			S++;
-			Money -= coins[Cents];
+			result++;
+			money -= coins[i];
 		}
 	}
 
-	printf("%d\n", S);
+	printf("%d\n", result);
 	return (0);
 }
+
