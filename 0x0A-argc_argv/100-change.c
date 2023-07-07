2beb1a485a6e43d449ab5e22606e973c3034218e
RevolverSnake412 <oussag@hotmail.fr>
2023-07-07 21:06:37 +0100

    changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,47 +6,35 @@
  * @argc: index
  * @argv: args' array
  *
- * Return: 1 if money less than a Penny, 0 on success
-*/
+ * Return: 1 if fails, 0 on success
+ */
 int main(int argc, char *argv[])
-{	int Quarter = 25, Dime = 10, Nickel = 5, Pair = 2, Penny = 1, Cents, S = 0;
+{
+	int Cents, S = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int Money = atoi(argv[1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) < 0)
-        {
-                printf("0\n");
-                return (0);
-        }
-	else
+	if (Money < 0)
 	{
-		for (Cents = 0; S != atoi(argv[1]); Cents++)
+		putchar(48);
+		putchar('\n');
+		return (0);
+	}
+
+	for (Cents = 0; Cents < 5 && Money >= 0; Cents++)
+	{
+		while (Money >= coins[Cents])
 		{
-			if (Quarter <= atoi(argv[1]) - S)
-			{
-				S += Quarter;
-			}
-			else if (Dime <= atoi(argv[1]) - S)
-			{
-				S += Dime;
-			}
-			else if (Nickel <= atoi(argv[1]) - S)
-			{
-				S += Nickel;
-			}
-			else if (Pair <= atoi(argv[1]) - S)
-			{
-				S += Pair;
-			}
-			else if (Penny == atoi(argv[1]) - S)
-			{
-				S += Penny;
-			}
+			S++;
+			Money -= coins[Cents];
 		}
 	}
-		printf("%d\n", Cents);
-		return (0);
+
+	printf("%d\n", S);
+	return (0);
 }
