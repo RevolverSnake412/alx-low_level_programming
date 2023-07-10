e15375724ccbf21a47c2293eab0cd8637cb326db
RevolverSnake412 <oussag@hotmail.fr>
2023-07-10 18:29:03 +0100

    alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - alloc_grid
+ *
+ * @width: int
+ *
+ * @height: int
+ *
+ * Return: ar on success
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ar;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	ar = malloc(sizeof(int *) * height);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ar[x] = malloc(sizeof(int) * width);
+		if (ar[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(ar[i]);
+			}
+			free(ar);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			ar[i][j] = 0;
+	}
+	return (ar);
+}
