09adfebb249629156efc4eac6e201d301421d82e
RevolverSnake412 <oussag@hotmail.fr>
2023-07-10 18:29:41 +0100

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		ar[x] = malloc(sizeof(int) * width);
+		ar[i] = malloc(sizeof(int) * width);
 		if (ar[i] == NULL)
 		{
 			for (; i >= 0; i--)
