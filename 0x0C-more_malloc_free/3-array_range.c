d240100327ae35a4f5cb365675dddfca3ed3c9ba
RevolverSnake412 <oussag@hotmail.fr>
2023-07-13 18:29:49 +0100

    arr range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - arr range
+ *
+ * @min: int
+ * @max: int
+ *
+ * Return: int_arr on success
+*/
+int *array_range(int min, int max)
+{
+	int *int_arr, i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	int_arr = malloc(sizeof(int) * (max - min + 1));
+
+	for (i = 0; min <= max; i++)
+	{
+		int_arr[i] = min;
+		min++;
+	}
+	return (int_arr);
+}
