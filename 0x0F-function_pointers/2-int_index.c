ad9cefef69cde78ee3f1aa48eb65bf622198285c
RevolverSnake412 <oussag@hotmail.fr>
2023-07-19 19:28:23 +0100

    prints array's element index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - int_index
+ *
+ * @array: int
+ * @size: int
+ * @cmp: pointer
+ *
+ * Return: -1 if fails
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
