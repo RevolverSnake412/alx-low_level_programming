d09f1b2d03e2e65209ac4dfb026d6fa8b8e913e4
RevolverSnake412 <oussag@hotmail.fr>
2023-07-19 20:02:04 +0100

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ *
+ * @argc: index
+ * @argv: arg's array
+ *
+ * Return: 0 on success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int num1, num2;
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (get_op_func(operator) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*operator == '/' && num2 == 0) ||
+	    (*operator == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(operator)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+/**
+ * op_add - sum
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
+/**
+ * op_sub - sub
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	int sub = a - b;
+	return (sub);
+}
+/**
+ * op_mul - mul
+ *
+ * @a: int
+ * @b: int.
+ *
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	int mul = a * b;
+	return (mul);
+}
+/**
+ * op_div - div
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	int div = a / b;
+	return (div);
+}
+/**
+ * op_mod - mod
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	int mod = a % b;
+	return (mod);
+}
