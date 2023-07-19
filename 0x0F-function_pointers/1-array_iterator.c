08552dcf76e29b71a76f238585ec2b67543c40fa
RevolverSnake412 <oussag@hotmail.fr>
2023-07-19 18:34:47 +0100

    array listing

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - array iteratir
+ *
+ * @array: int
+ * @size: size_t
+ * @action: pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	
+	for (i = 0; i <= size - 1; i++)
+	{
+		action(array[i]);
+	}
+}
