2a7f4518bb0acc276e4923f16e8700b3477be0a4
RevolverSnake412 <oussag@hotmail.fr>
2023-07-20 19:27:10 +0100

    Prints nearly all types

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - print_all
+ *
+ * @format: const char
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *string;
+
+	va_list args;
+
+	va_start(args, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					string = va_arg(args, char *);
+					if (string == NULL)
+						string = "(nil)";
+					printf("%s", string);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			i++;
+			while(format[i] != '\0')
+			{
+				printf(", ");
+				break;
+			}
+		}
+	}
+	putchar('\n');
+	va_end(args);
+}
