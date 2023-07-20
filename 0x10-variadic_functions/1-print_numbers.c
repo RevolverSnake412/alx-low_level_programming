325c4e4723ce39f8d20437c591b840288190b615
RevolverSnake412 <oussag@hotmail.fr>
2023-07-20 17:50:31 +0100

    Prints numbers in its parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints the numbers in parameters
+ *
+ * @separator: const char
+ * @n: const unsigned int
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+
+		if (i != n - 1 && separator != NULL)
+			printf("%s ", separator);
+	}
+	putchar('\n');
+	va_end(args);
+}
