0e8d99ab56bc194b31a1e4ce0ef64c565e8b09eb
RevolverSnake412 <oussag@hotmail.fr>
2023-07-27 17:01:57 +0100

    Commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints a linked list
+ * @h: linked list
+ *
+ * Return: 
+*/
+size_t print_list(const list_t *h)
+{
+	size_t element = 0;
+
+	while (h)
+	{
+		if (h->str)
+            printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		element++;
+	}
+
+	return (element);
+}
