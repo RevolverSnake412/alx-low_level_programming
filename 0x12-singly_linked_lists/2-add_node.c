e4e5f226d1c3801888e2b1543f72c387f6156eb0
RevolverSnake412 <oussag@hotmail.fr>
2023-07-27 18:38:09 +0100

    Update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
  * add_node - adds new node
  * @head: list_t
  * @str: const char
- * 
+ *
  * Return: new
 */
 list_t *add_node(list_t **head, const char *str)
@@ -13,19 +13,19 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	unsigned int len = 0;
 
-    new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 
 	while (str[len])
 		len++;
 
 	if (!new)
 		return (NULL);
-    else
-    {
-	    new->str = strdup(str);
-	    new->len = len;
-	    new->next = (*head);
-	    (*head) = new;
-    }
+	else
+	{
+		new->str = strdup(str);
+		new->len = len;
+		new->next = (*head);
+		(*head) = new;
+	}
 	return (new);
 }
