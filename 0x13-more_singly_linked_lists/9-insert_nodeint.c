d4be2ae7326c66db866c1f259a384eaa24a777a3
RevolverSnake412 <oussag@hotmail.fr>
2023-08-01 00:53:53 +0100

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new;
 		return (new);
 	}
-	for (i = 0; i < idx; i++)
+	for (i = 0; first && i < idx; i++)
 	{
 		if (idx == i + 1)
 		{
