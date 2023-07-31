8451df90a95cda33d5d15d156edef28ffa8fabd9
RevolverSnake412 <oussag@hotmail.fr>
2023-08-01 00:03:39 +0100

    returns the sum of all nodes in a linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of all nodes
+ * @head: linked list
+ *
+ * Return: sum;
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0, n;
+
+	if (!head)
+		return (0);
+
+	while(head)
+	{
+		n = head->n;
+		head = head->next;
+		sum += n;
+	}
+
+	return (sum);
+}
