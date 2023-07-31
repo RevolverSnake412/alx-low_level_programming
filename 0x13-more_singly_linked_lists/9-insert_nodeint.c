c3c538ac945d724a00745015064a305f1f614d38
RevolverSnake412 <oussag@hotmail.fr>
2023-08-01 00:52:02 +0100

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: linked list
- * @ind: index
+ * @idx: index
  * @n: node
  *
  * Return: new if success, NULL if failure
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = (*head);
 		(*head) = new;
-		return (*head);
+		return (new);
 	}
 	for (i = 0; i < idx; i++)
 	{
