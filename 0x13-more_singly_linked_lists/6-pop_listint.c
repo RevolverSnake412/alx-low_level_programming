375f0bc81bbe90e08dba33bda7f949b9a6ce20f8
RevolverSnake412 <oussag@hotmail.fr>
2023-07-31 23:06:40 +0100

    Update

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,11 +10,8 @@ int pop_listint(listint_t **head)
 	listint_t *new;
 	int n = (*head)->n;
 
-	while (*head)
-	{
-		new = (*head)->next;
-		(*head) = new;
-	}
+	new = (*head)->next;
+	(*head) = new;
 
 	return (n);
 }
