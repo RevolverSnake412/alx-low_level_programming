7ccc5572f14fb7ca80ead476437252008b72cb8a
RevolverSnake412 <oussag@hotmail.fr>
2023-07-31 23:38:14 +0100

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *node = head;
 
-	if (!node)
+	if (!head)
 		return (NULL);
 
-	for (i = 0; i <= index; i++)
+	for (i = 0; i < index; i++)
 	{
 		node = head;
 		head = head->next;
