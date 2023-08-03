3979fb0041fe70bd8cabfd98948b8ef7d01b7516
RevolverSnake412 <oussag@hotmail.fr>
2023-08-03 19:46:54 +0100

    Returns the bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - get_bit
+ * @n: provided number
+ * @index: index
+ *
+ * Return: -1 if fails, bit on success
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8) 
+		return -1;
+
+	return (n & mask) ? 1 : 0;
+/* OwO */
+}
