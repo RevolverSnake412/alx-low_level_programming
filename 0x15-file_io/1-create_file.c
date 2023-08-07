9f73fda9aa9dfe2d203594a0646371398bb4822f
RevolverSnake412 <oussag@hotmail.fr>
2023-08-07 19:04:56 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || file == -1)
 		return (-1);
 
-	while (text_content[size] != '\0' && text_content != NULL)
-		size++;
+	if (text_content != NULL)
+		while (text_content[size])
+			size++;
 
 	written = write(file, text_content, size);
 
