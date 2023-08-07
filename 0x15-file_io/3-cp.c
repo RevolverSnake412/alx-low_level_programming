34003ee2310cba0ca273d4b39068be75dab84089
RevolverSnake412 <oussag@hotmail.fr>
2023-08-07 21:00:41 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,13 +21,13 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-		return(97);
+		return (97);
 	}
 
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		return(99);
+		return (99);
 	}
 
 	while ((size = read(fd_from, buffer, sizeof(buffer))) > 0)
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 
 		if (written == -1)
 		{
-            		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			return (99);
 		}
 	}
