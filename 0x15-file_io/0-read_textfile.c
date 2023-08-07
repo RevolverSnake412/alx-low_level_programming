d54902a2d87d08566f0941cedc80425aa6486fac
RevolverSnake412 <oussag@hotmail.fr>
2023-08-07 17:23:15 +0100

    reads and prints

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+/**
+ * read_textfile - prints what it reads, bc why not? this is a free country!
+ * @filename: file
+ * @letters: size of string to print
+ *
+ * Return: size on success, 0 on failure
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file = open(filename, O_RDONLY);
+	char *buffer = (char *)malloc(letters + 1);
+	ssize_t size = read(file, buffer, letters);
+
+	if (file == -1 || filename == NULL)
+		return (0);
+
+	buffer[size] = '\0';
+
+	write(1, buffer, size);
+
+	close(file);
+	free(buffer);
+	return (size);
+}
