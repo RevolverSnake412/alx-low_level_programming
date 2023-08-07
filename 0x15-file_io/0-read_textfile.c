d123d52820f8df1127e706be40730018a7d4add7
RevolverSnake412 <oussag@hotmail.fr>
2023-08-07 17:49:28 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,10 +18,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	written = write(1, buffer, size);
-	if (written == -1 || size != written)
-		return (0);
 
 	close(file);
 	free(buffer);
+
 	return (size);
 }
