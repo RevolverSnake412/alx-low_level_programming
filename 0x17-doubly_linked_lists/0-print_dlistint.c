f3073a17cb370ecb3c7d0caf3ca57a002a5d603d
RevolverSnake412 <oussag@hotmail.fr>
2023-09-14 19:58:08 +0100

    TASK 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - prints the elements
+ * @h: head
+ *
+ * Return: count
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
