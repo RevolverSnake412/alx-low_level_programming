9d81e6f139f839b8ca2cf097873fd688ec917401
RevolverSnake412 <oussag@hotmail.fr>
2023-09-14 20:50:20 +0100

    TASK 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - frees a list
+ * @head: head
+*/
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+
+	free(head);
+}
