359d70055265b0b666e657fda57a7f2a21b16fa8
RevolverSnake412 <oussag@hotmail.fr>
2023-09-14 20:07:30 +0100

    TASK 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 #include <stdio.h>
 /**
+ * dlistint_len - counts elements
+ * @h: head
  *
+ * Return: count
 */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count = 0;
+
+	if (h == NULL)
+		return (count);
 
 	while (h->prev != NULL)
 		h = h->prev;
