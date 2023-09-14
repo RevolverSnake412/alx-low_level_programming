9b69dec55ff4bd027f8bd5838576027fd4bab6c6
RevolverSnake412 <oussag@hotmail.fr>
2023-09-14 21:16:27 +0100

    update

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * sum_dlistint - sum of all nodes' elements (integers)
  * @head: head
@@ -7,13 +8,18 @@
 */
 int sum_dlistint(dlistint_t *head)
 {
-	int S = 0, n;
+	int S = 0;
 
 	while (head->prev != NULL)
-	{
-		n = head->n;
 		head = head->prev;
-		S += n;
+
+	S = head->n;
+
+	while (head->next != NULL)
+	{
+		head = head->next;
+		S += head->n;
 	}
+
 	return (S);
 }
