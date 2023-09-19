232dc80a6433e2892b86216f17b7feb6837759a1
RevolverSnake412 <oussag@hotmail.fr>
2023-09-19 01:14:50 +0100

    rm

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/RevolverSnake412/alx-low_level_programming/blob/main/0x18-dynamic_libraries/fakerand.so
+wget -P /tmp  
 export LD_PRELOAD=/tmp/putshack.so
