d263e1d636bfd444acbee559b0e5330e555a61ef
RevolverSnake412 <oussag@hotmail.fr>
2023-10-06 21:29:05 +0100

    betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,10 +1,7 @@
 #include "hash_tables.h"
 /**
- * hash_table_print - Prints a hash table.
- * @ht: A pointer to the hash table to print.
- *
- * Description: Key/value pairs are printed in the order
- *              they appear in the array of the hash table.
+ * hash_table_print - rints a hash table
+ * @ht: the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
 {
@@ -16,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 		return;
 
 	printf("{");
-	
+
 	flag = 0;
 	for (; i < ht->size; i++)
 	{
