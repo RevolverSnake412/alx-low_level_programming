4b28e39212d66f246bc78489c23f8c2ce963bb6e
RevolverSnake412 <oussag@hotmail.fr>
2023-10-06 18:43:08 +0100

    Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,18 +1,20 @@
 #include "hash_tables.h"
-
 /**
+ * hash_table_create - creates a new hash table
+ * @size: the size of the hash table
  *
+ * Return: always returns the table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i = 0;
-    hash_table_t *hash_table = malloc(sizeof(hash_node_t));
-    
-    hash_table->size = size;
-    hash_table->array = calloc(hash_table->size, sizeof(size));
-    
-    for (; i < hash_table->size; i++)
-        hash_table->array[i] = NULL;
+	unsigned long int i = 0;
+	hash_table_t *hash_table = malloc(sizeof(hash_node_t));
 
-    return hash_table;
+	hash_table->size = size;
+	hash_table->array = calloc(hash_table->size, sizeof(size));
+
+	for (; i < hash_table->size; i++)
+		hash_table->array[i] = NULL;
+
+	return (hash_table);
 }
