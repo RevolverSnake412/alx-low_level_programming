81ce585788881cc5fc2113acfaca18423b6ec396
RevolverSnake412 <oussag@hotmail.fr>
2023-10-06 20:49:24 +0100

    TASK 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - a function that retrieves a value associated with a key.
+ * @ht: the hash table
+ * @key: the key
+ *
+ * Return: key on success, NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *new_node;
+
+	if (!ht || !key)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	if (idx >= ht->size)
+		return (NULL);
+
+	new_node = ht->array[idx];
+
+	while (strcmp(new_node->key, key) != 0 && new_node != NULL)
+		new_node = new_node->next;
+
+	return ((!new_node) ? NULL : new_node->value);
+}
