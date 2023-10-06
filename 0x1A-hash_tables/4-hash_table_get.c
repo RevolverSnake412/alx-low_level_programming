4d29ff7fda2fa49f1aaa5c0e5d2b7e82f0012b17
RevolverSnake412 <oussag@hotmail.fr>
2023-10-06 21:38:36 +0100

    skull

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_get - a function that retrieves a value associated with a key.
  * @ht: the hash table
@@ -22,7 +21,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	new_node = ht->array[idx];
 
-	while (strcmp(new_node->key, key) != 0 && new_node)
+	while (new_node && strcmp(new_node->key, key) != 0)
 		new_node = new_node->next;
 
 	return ((new_node == NULL) ? NULL : new_node->value);
