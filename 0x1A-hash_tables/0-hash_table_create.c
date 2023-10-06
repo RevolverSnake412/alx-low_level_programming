c1cc81cb40c8bbc92c11afa6e36ba537326a8d17
RevolverSnake412 <oussag@hotmail.fr>
2023-10-06 18:52:10 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 	hash_table_t *hash_table = malloc(sizeof(hash_node_t));
 
+	if (!hash_table)
+		return (NULL);
+
 	hash_table->size = size;
-	hash_table->array = calloc(hash_table->size, sizeof(size));
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	for (; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
