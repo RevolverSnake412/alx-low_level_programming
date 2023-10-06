a0eb348c6e3cae060bcaf4ac3381829aed12d8c5
RevolverSnake412 <oussag@hotmail.fr>
2023-10-06 18:24:46 +0100

    TASK 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ *
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hash_table = malloc(sizeof(hash_node_t));
+
+    hash_table->size = size;
+    return hash_table;
+}
