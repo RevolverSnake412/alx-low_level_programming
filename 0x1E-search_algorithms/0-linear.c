9bc53001641c96b894d2a42be08c291fa5742832
RevolverSnake412 <oussag@hotmail.fr>
2024-01-15 18:34:17 +0000

    betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,19 +2,28 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
+/**
+ * linear_search - searches for value in a sequential method.
+ *
+ * @array: an unsorted/sorted array.
+ * @size: the size of the array.
+ * @value: the value to search for
+ *
+ * Return: index if found, otherwise -1
+*/
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    for (; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
